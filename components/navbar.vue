a9c0f25f0d0d1e8d5ec0deec24d54c4007aa0848
Fitywe <96216623+Pure227@users.noreply.github.com>
2024-05-13 16:23:50 +0700

    first commit

---
+++ components/navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <header class="lg:flex flex justify-between bg-[#FFFFFF] h-[100px] items-center drop-shadow-md">
+            <nav class="navbar-logo"> 
+                <img src="/img/grit.png" class="h-[75.63px]" alt="">
+            </nav>
+            <nav class="box-border h-[100px] w-[335px] flex items-center space-x-8">
+                <i class="pi pi-bell" style="font-size: 2rem"></i>
+                <img src="/img/profile.png" class="h-[63px] w-[63px]" alt="">
+                <li class="flex flex-col">
+                    <strong class="mb-2">Peter boom</strong>
+                    <span>แอดมิน</span>
+                </li>
+                <i class="pi pi-sort-down" style="font-size: 1rem"></i>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-[#E2E2E2] min-h-screen">
+    <div>
+      <navbar />
+      <div class="max-w-screen-xl flex gap-2">
+        <sidebar />
+        <div class="w-full">
+            <slot/>
+        </div>
+      </div>
+    </div>      
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+// header {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 10px;
+//     background-color: white;
+//     color: black;
+// }
+
+// .navbar-logo {
+//     display: flex;
+//     align-items: center;
+// }
+
+// .navbar-logo img {
+//     height: 50px;
+//     margin-right: 10px;
+// }
+
+// // .content-container {
+// //     padding: 20px;
+// // }
+</style>
